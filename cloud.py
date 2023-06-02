161d759dd2c337d38da4beb85440ea99008a5e43
RongHua Wang <ronghua.art@gmail.com>
2023-06-02 17:20:18 -0500

    old code

--- cloud.py
+++ cloud.py
@@ -2,41 +2,48 @@ import bpy
 import random 
 import math
 
-span = 10 
-radius = 2
-decay_rad = 5/10
+startSpan = 0
+endSpan = 5
 
-min_segment = 4 
-growth_seg = 3/2
+edgeSize = .5
+centerSize = 2
 
-s = bpy.ops.mesh.primitive_uv_sphere_add(segments=10, ring_count=10, radius=0.1)
+bpy.ops.mesh.primitive_uv_sphere_add()
 main_obj = bpy.context.active_object
 main_obj.location[2] = 1
 
 
 for i in range(100):  
+    size = 2
+    percent = 5/10
+    
+    
+    bpy.ops.mesh.primitive_uv_sphere_add()
+    loop_obj = bpy.context.active_object
+    
+    span = 10
+    height = 5
     
     x_val = random.random() * span - (span/2)
     y_val = random.random() * span - (span/2)
+    
     xy_avg = (abs(x_val) + abs(y_val))/2
     
-    # y = ab^x (a is size, b percent, x is xy_avg)
-    # (x is xy_avg; y is the size/radius)
-    radius = radius * (decay_rad ** xy_avg)
+    #y = ab^x (a is size, b percent, x is xy_avg)
     
-    z_val = radius + ((random.random() * radius) - (radius/2))
-
-    # y = ab^x (a is the min segment; b is percentage (from 1 to 1.5)
-    # (x is size; y gives us the respective segment)
-
-    seg_val = int(min_segment * (growth_seg ** radius))
-
-    bpy.ops.mesh.primitive_uv_sphere_add(segments=seg_val, ring_count=seg_val, radius=radius)
-    loop_obj = bpy.context.active_object
+    size = size * (percent ** xy_avg)
+    
+    z_val = size + ((random.random() * size) - (size/2))
     
     loop_obj.location[0] = x_val
     loop_obj.location[1] = y_val
     loop_obj.location[2] = z_val
+    
+    
+    
+    loop_obj.scale[0] = size
+    loop_obj.scale[1] = size 
+    loop_obj.scale[2] = size
     
     mod_bool = loop_obj.modifiers.new("boolean", 'BOOLEAN') 
     mod_bool.operation = 'UNION'

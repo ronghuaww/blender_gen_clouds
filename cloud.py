6a2363a4187a880eeb452be0d8e54002b339db0e
RongHua Wang <ronghua.art@gmail.com>
2023-06-03 00:07:25 -0500

    seg fixed

--- cloud.py
+++ cloud.py
@@ -3,27 +3,28 @@ import random
 import math
 
 span = 10 
-radius = 2
 decay_rad = 5/10
 
-min_segment = 4 
-growth_seg = 3/2
+min_segment = 3
+growth_seg = 3
 
-s = bpy.ops.mesh.primitive_uv_sphere_add(segments=10, ring_count=10, radius=0.1)
+midpoint_x = 2
+midpoint_y = 2
+
+s = bpy.ops.mesh.primitive_uv_sphere_add(segments=4, ring_count=4, radius=0.2)
 main_obj = bpy.context.active_object
 main_obj.location[2] = 1
 
 
-for i in range(100):  
-    
+for i in range(100):     
     x_val = random.random() * span - (span/2)
     y_val = random.random() * span - (span/2)
-    xy_avg = (abs(x_val) + abs(y_val))/2
+    xy_avg = (abs(x_val + midpoint_x) + abs(y_val + midpoint_y))/2
     
     # y = ab^x (a is size, b percent, x is xy_avg)
     # (x is xy_avg; y is the size/radius)
+    radius = 2 
     radius = radius * (decay_rad ** xy_avg)
-    
     z_val = radius + ((random.random() * radius) - (radius/2))
 
     # y = ab^x (a is the min segment; b is percentage (from 1 to 1.5)

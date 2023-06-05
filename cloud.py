cd5ef58567df9499b195420d79e0198520ae25b5
RongHua Wang <ronghua.art@gmail.com>
2023-06-04 23:37:07 -0500

    added panel; but so slow :((

--- cloud.py
+++ cloud.py
@@ -2,56 +2,124 @@ import bpy
 import random 
 import math
 
-<<<<<<< HEAD
-span = 10 
-decay_rad = 5/10
+class MESH_sphere_clouds(bpy.types.Operator):
+    """lots of spheres for some fluffy clouds"""
+    bl_idname = "mesh.sphere_clouds"
+    bl_label = "Sphere Clouds"
+    bl_options = {'REGISTER', 'UNDO'}
 
-min_segment = 3
-growth_seg = 3
+    num_spheres: bpy.props.IntProperty(
+        name="Number of Spheres", 
+        description="Number of Spheres",
+        default=50
+    )
 
-midpoint_x = 2
-midpoint_y = 2
+    span_x: bpy.props.FloatProperty(
+        name="X", 
+        description="Length of the Clouds in the X-direction", 
+        default=10
+    )
 
-s = bpy.ops.mesh.primitive_uv_sphere_add(segments=4, ring_count=4, radius=0.2)
-main_obj = bpy.context.active_object
-main_obj.location[2] = 1
+    span_y: bpy.props.FloatProperty(
+        name="Y", 
+        description="Length of the Clouds in the Y-direction", 
+        default=5
+    )
 
-for i in range(100):     
-    x_val = random.random() * span - (span/2)
-    y_val = random.random() * span - (span/2)
-    xy_avg = (abs(x_val + midpoint_x) + abs(y_val + midpoint_y))/2
-    
-    # y = ab^x (a is size, b percent, x is xy_avg)
-    # (x is xy_avg; y is the size/radius)
-    radius = 2 
-    radius = radius * (decay_rad ** xy_avg)
-    z_val = radius + ((random.random() * radius) - (radius/2))
+    radius: bpy.props.IntProperty(
+        name="Maximum Radius", 
+        description="Maximum Radius of the Spheres",
+        default=2
+    )
 
-    # y = ab^x (a is the min segment; b is percentage (from 1 to 1.5)
-    # (x is size; y gives us the respective segment)
+    decay_rad: bpy.props.FloatProperty(
+        name="Radius Decay Factor", 
+        description="Exponential Decay of the radius as it gets further from the center", 
+        default= 1/2,
+        min=0, max= 1
+    )
 
-    seg_val = int(min_segment * (growth_seg ** radius))
+    min_segment: bpy.props.IntProperty(
+        name="Segment", 
+        description="Minmum number of Segments for Each Sphere",
+        default=3
+    )
 
-    bpy.ops.mesh.primitive_uv_sphere_add(segments=seg_val, ring_count=seg_val, radius=radius)
-    loop_obj = bpy.context.active_object
-    
-    loop_obj.location[0] = x_val
-    loop_obj.location[1] = y_val
-    loop_obj.location[2] = z_val
-    
-    
-    
-    loop_obj.scale[0] = size
-    loop_obj.scale[1] = size 
-    loop_obj.scale[2] = size
-    
-    mod_bool = loop_obj.modifiers.new("boolean", 'BOOLEAN') 
-    mod_bool.operation = 'UNION'
-    mod_bool.object = main_obj
-    
-    bpy.ops.object.modifier_apply(modifier="boolean")
-    
-    loop_obj.select_set(False)
-    main_obj.select_set(True)
-    bpy.ops.object.delete()
-    main_obj = loop_obj
+    growth_seg: bpy.props.FloatProperty(
+        name="Segment Growth Factor", 
+        description="Exponential Growth in the number of segments as it gets further from the center", 
+        default=3,
+        min=1, soft_max=10
+    )
+
+    midpoint_x: bpy.props.FloatProperty(
+        name="Midpoint X-Coordinate", 
+        description="X-Coordinate for where the radius is the highest",
+        default=2
+    )
+
+    midpoint_y: bpy.props.FloatProperty(
+        name="Midpoint Y-Corrdinate", 
+        description="Y-Coordinate for where the radius is the highest",
+        default=3
+    )
+
+
+    def execute (self, context): 
+        # default circle where all the new ones are merge with
+        s = bpy.ops.mesh.primitive_uv_sphere_add(
+            segments=4, 
+            ring_count=4, 
+            radius=0.2)
+        main_obj = bpy.context.active_object
+        main_obj.location[2] = 1
+
+        for i in range(self.num_spheres):     
+            x_val = random.random() * self.span_x - (self.span_x/2)
+            y_val = random.random() * self.span_y - (self.span_y/2)
+            xy_avg = (abs(x_val + self.midpoint_x) + abs(y_val + self.midpoint_y))/2
+            
+            # y = ab^x (a is size, b percent, x is xy_avg)
+            # (x is xy_avg; y is the size/radius)
+            rad = 2
+            rad = rad * (self.decay_rad ** xy_avg)
+            z_val = rad + ((random.random() * (rad * 2)) - (rad))
+
+            # y = ab^x (a is the min segment; b is percentage (from 1 to 1.5)
+            # (x is size; y gives us the respective segment)
+            seg_val = int(self.min_segment * (self.growth_seg ** rad))
+
+            bpy.ops.mesh.primitive_uv_sphere_add(
+                segments=seg_val, 
+                ring_count=seg_val, 
+                radius=rad)
+            loop_obj = bpy.context.active_object
+            
+            loop_obj.location[0] = x_val
+            loop_obj.location[1] = y_val
+            loop_obj.location[2] = z_val
+            
+            mod_bool = loop_obj.modifiers.new("boolean", 'BOOLEAN') 
+            mod_bool.operation = 'UNION'
+            mod_bool.object = main_obj
+            
+            bpy.ops.object.modifier_apply(modifier="boolean")
+            
+            loop_obj.select_set(False)
+            main_obj.select_set(True)
+            bpy.ops.object.delete()
+            main_obj = loop_obj
+
+        return {'FINISHED'}
+        
+def register(): 
+    bpy.utils.register_class(MESH_sphere_clouds)
+
+def unregister():
+    bpy.utils.unregister_class(MESH_sphere_clouds)
+
+# why does this not work?? 
+#if __name__ == '__main__':
+#   register()
+
+bpy.utils.register_class(MESH_sphere_clouds)
